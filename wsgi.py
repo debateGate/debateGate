543b5afe67220f8afe4cc20bb3ea1a1dc3767c3c
threadNecromancer <sebastiancbenjamin@gmail.com>
2017-08-03 11:24:52 -0700

    Main -> app change in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from app.main import main as app
 if __name__ == '__main__':
-    if main.config["DEBUG"] == True:
+    if app.config["DEBUG"] == True:
         app.run(host="localhost", ssl_context=('./ssl.crt', './ssl.key'))
     else:
         app.run()
