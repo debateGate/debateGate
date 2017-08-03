c910899b3fdddb83c0768309ba1b682d250cd1ab
threadNecromancer <sebastiancbenjamin@gmail.com>
2017-08-03 11:32:16 -0700

    Fix wsgi.py to work with new openssl methods and add those new fileendings to .gitignore

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from app.main import main as app
 if __name__ == '__main__':
     if app.config["DEBUG"] == True:
-        app.run(host="localhost", ssl_context=('./ssl.crt', './ssl.key'))
+        app.run(host="localhost", ssl_context=('./cert.pem', './key.pem'))
     else:
         app.run()
